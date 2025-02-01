d38ab0a6201abb76e4b9931d4217cc2362db49e5
SupremeRubisco <eythanlawless@mac.lan>
2025-01-31 20:36:10 -0500

    Added More and Fixed some styling issues!

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let stupid = 'true';
+let stupid = true;
 //What are you doing here?
 console.log('Someone is digging around');
 //https://www.youtube.com/watch?v=dQw4w9WgXcQ
-if (stupid=='true'){
+if (stupid){
     console.log('You are stupid');
 }
